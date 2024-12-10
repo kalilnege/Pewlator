b7efc4a6deb82b59675fbe14ddb8af45e64a3afc
kalilnege <130768837+kalilnege@users.noreply.github.com>
2024-12-10 14:00:06 -0300

    Delete server directory

--- server/models/Avaliador.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AvaliadorSchema = new mongoose.Schema({
-    nome: { type: String, required: true },
-    matricula: { type: String, required: true },
-    funcao: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Avaliador', AvaliadorSchema);
